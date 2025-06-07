9300712c05fdc3c0107ab8a74063481769cba21d
Lucas Aoki <lucas.aoki2001@gmail.com>
2025-06-06 21:02:04 -0300

    finalizaçao da funçao limpar com todos os elementos

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 function limpar(){
     document.getElementById("nome").value = "";
-    document.getElementById("").value = "";
-    document.getElementById("").value = "";
-    document.getElementById("").value = "";
-    document.getElementById("").value = "";
-    document.getElementById("").value = "";
-    document.getElementById("").value = "";
-    document.getElementById("").value = "";
-    document.getElementById("").value = "";
-    document.getElementById("").value = "";
-
+    document.getElementById("titulo").value = "";
+    document.getElementById("descricao").value = "";
+    document.getElementById("contato").value = "";
+    document.getElementById("cep").value = "";
+    document.getElementById("rua").value = "";
+    document.getElementById("bairro").value = "";
+    document.getElementById("cidade").value = "";
+    document.getElementById("estado").value = "";
 }
